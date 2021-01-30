f9cf234fc5e954be2c64881855844a0a0c69f206
uHan <asgd56@gmail.com>
2021-01-30 13:28:18 +0900

    first commit

---
+++ src/main/java/com/thestat/thestat/TheStatApplication.java
@@ -0,0 +1,13 @@
+package com.thestat.thestat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheStatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheStatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thestat/thestat/TheStatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thestat.thestat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheStatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
