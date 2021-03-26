db004f44625fe86572488616d08d186f28ccb0be
uhan <asgd56@gmail.com>
2021-03-26 13:43:01 +0900

    feat: implement basic controller and edit index page

---
+++ src/main/java/com/thestat/thestat/controller/IndexController.java
@@ -0,0 +1,38 @@
+package com.thestat.thestat.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController
+{
+    @GetMapping("/")
+    public String root()
+    {
+        return "index";
+    }
+
+    @GetMapping("/about")
+    public String about()
+    {
+        return "about";
+    }
+
+    @GetMapping("/services")
+    public String services()
+    {
+        return "services";
+    }
+
+    @GetMapping("/contact")
+    public String contact()
+    {
+        return "contact";
+    }
+
+    @GetMapping("/404")
+    public String error()
+    {
+        return "404";
+    }
+}
